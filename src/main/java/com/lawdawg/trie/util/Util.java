4b50feffaf05782be7df67269a654362f6de4978
Andrew Baine <andrew.baine@gmail.com>
2012-05-26 16:40:42 -0700

    use buffered input stream

--- src/main/java/com/lawdawg/trie/util/Util.java
+++ src/main/java/com/lawdawg/trie/util/Util.java
@@ -1,5 +1,6 @@
 package com.lawdawg.trie.util;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,8 +9,13 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Util {
 
+	private static final Logger logger = LoggerFactory.getLogger(Util.class);
+	
 	public static void barf(final ByteBuffer bb, final OutputStream out) throws IOException {
 		final DataOutputStream dout = new DataOutputStream(out);
 		final int remaining = bb.remaining();
@@ -18,8 +24,9 @@ public class Util {
 	}
 	
 	public static byte[] slurp(final InputStream in) throws IOException {
-		final DataInputStream din = new DataInputStream(in);
+		final DataInputStream din = new DataInputStream(new BufferedInputStream(in));
 		final int n = din.readInt();
+		logger.info("reading {} bytes", n);
 		final byte[] data = new byte[n];
 		for (int i = 0; i < n; i++) {
 			data[i] = din.readByte();
